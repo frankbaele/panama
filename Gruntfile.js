b2df8b7b6be8aaeaea8a9facdce44d9e42bf2fcb
Frank Baele <frankbaele@gmail.com>
2013-05-16 13:15:54 +0200

    add index
    Former-commit-id: b0336bba3bcf25484535b4a1eb78028f1ce34f34

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,142 +1,91 @@
-/*global module:false, process:false*/
-module.exports = function(grunt) {
+/*
+ * grunt
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2013 "Cowboy" Ben Alman
+ * Licensed under the MIT license.
+ * https://github.com/gruntjs/grunt/blob/master/LICENSE-MIT
+ */
 
-  'use strict';
+'use strict';
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    nodeunit: {
+      all: ['test/{grunt,tasks,util}/**/*.js']
+    },
     jshint: {
-      test: [
-        'src/**/*.js',
-        'specs/**/*.js',
-      ],
-      docs: [
-        'src/**/*.js'
-      ],
-      examples: [
-        'src/**/*.js',
-        'specs/**/*.js',
-        'examples/**/*.js',
-        '!examples/scripts/**/*.js'
-      ],
-      all: [
-        'Gruntfile.js',
-        'src/**/*.js',
-        'specs/**/*.js',
-        'examples/**/*.js',
-        '!examples/scripts/**/*.js'
-      ],
+      gruntfile: ['Gruntfile.js'],
+      libs_n_tests: ['lib/**/*.js', '<%= nodeunit.all %>'],
+      subgrunt: ['<%= subgrunt.all %>'],
       options: {
-        jshintrc: '.jshintrc'
-      }
-    },
-    connect: {
-      test: {},
-      docs: {},
-      examples: {},
-      all: {
-        options: {
-          keepalive: true
-        }
-      },
-      options: {
-        hostname: '0.0.0.0',
-        port: 8000,
-        keepalive: false
-      }
-    },
-    open: {
-      test: {
-        path: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>/_SpecRunner.html'
-      },
-      docs: {
-        path: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>/docs/'
-      },
-      examples: {
-        path: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>/examples/'
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        node: true,
+        es5: true
       }
     },
     watch: {
-      test: {
-        files: '<%= jshint.test %>',
-        tasks: ['jshint:test', 'jasmine:all', 'jasmine:all:build', 'open:test']
+      gruntfile: {
+        files: ['<%= jshint.gruntfile %>'],
+        tasks: ['jshint:gruntfile']
       },
-      docs: {
-        files: '<%= jshint.docs %>',
-        tasks: ['jshint:docs', 'jsdoc:all', 'copy:docs']
+      libs_n_tests: {
+        files: ['<%= jshint.libs_n_tests %>'],
+        tasks: ['jshint:libs_n_tests', 'nodeunit']
       },
-      examples: {
-        files: '<%= jshint.examples %>',
-        tasks: ['jshint:examples', 'dojo']
-      },
-      all: {
-        files: '<%= jshint.all %>',
-        tasks: ['build']
+      subgrunt: {
+        files: ['<%= subgrunt.all %>'],
+        tasks: ['jshint:subgrunt', 'subgrunt']
       }
     },
-    dojo: {
-      frozen: {},
-      options: {
-        dojo: 'deps/dojo/dojo.js',
-        profile: 'frozen.profile.js',
-        'package': './',
-        dojoConfig: 'dojoConfig.js',
-        cwd: './'
-      }
+    subgrunt: {
+      all: ['test/gruntfile/*.js']
     },
-    jsdoc: {
-      all: {
-        src: ['src/**/*.js', 'package.json', 'README.md'],
-        dest: 'docs',
-        options: {
-          configure: 'jsdoc_config.json',
-          'private': false,
-          template: process.cwd() + '/node_modules/frozen-jsdoc-template'
-        }
-      }
-    },
-    copy: {
-      docs: {
-        files: [
-          { dest: 'examples/index.html', src: 'docs/examples.html' },
-          { expand: true, cwd: 'docs/', dest: 'examples/', src: ['styles/*.css', 'scripts/*.js'] }
-        ]
-      }
-    },
-    jasmine: {
-      all: {
-        options: {
-          specs: 'specs/**/*Spec.js',
-        }
-      },
-      options: {
-        src: 'src/**/*.js',
-        helpers: [
-          'deps/Box2d.min.js',
-          'dojoConfig.js',
-          'deps/dojo/dojo.js'
-        ],
-        host: 'http://127.0.0.1:8000/',
-        template: 'specs/fixtures/_SpecRunner.tmpl'
-      }
-    }
   });
 
-  grunt.loadNpmTasks('grunt-dojo');
-  grunt.loadNpmTasks('grunt-open');
-  grunt.loadNpmTasks('grunt-jsdoc');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // "npm test" runs these tasks
+  grunt.registerTask('test', ['jshint', 'nodeunit', 'subgrunt']);
 
   // Default task.
-  grunt.registerTask('default', ['jshint:all', 'connect:test', 'jasmine:all']);
+  grunt.registerTask('default', ['test']);
 
-  grunt.registerTask('build', ['jshint:all', 'connect:test', 'jasmine:all', 'jsdoc:all', 'dojo']);
-  grunt.registerTask('test', ['jshint:test', 'connect:test', 'jasmine:all', 'jasmine:all:build', 'open:test', 'watch:test']);
-  grunt.registerTask('docs', ['jshint:docs', 'jsdoc:all', 'copy:docs', 'connect:docs', 'open:docs', 'watch:docs']);
-  grunt.registerTask('examples', ['jshint:examples', 'dojo', 'connect:examples', 'open:examples', 'watch:examples']);
+  // Run sub-grunt files, because right now, testing tasks is a pain.
+  grunt.registerMultiTask('subgrunt', 'Run a sub-gruntfile.', function() {
+    var path = require('path');
+    grunt.util.async.forEachSeries(this.filesSrc, function(gruntfile, next) {
+      grunt.util.spawn({
+        grunt: true,
+        args: ['--gruntfile', path.resolve(gruntfile)],
+      }, function(error, result) {
+        if (error) {
+          grunt.log.error(result.stdout).writeln();
+          next(new Error('Error running sub-gruntfile "' + gruntfile + '".'));
+        } else {
+          grunt.verbose.ok(result.stdout);
+          next();
+        }
+      });
+    }, this.async());
+  });
 
 };
