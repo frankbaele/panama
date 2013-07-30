b65e64f821a96b68259d36ded407f3fcf11890d8
Frank Baele <frankbaele@gmail.com>
2013-07-30 08:39:47 +0200

    moved socket connection code to seperate file and create empty grid

--- server/js/world.js
+++ server/js/world.js
@@ -1,10 +1,23 @@
 var World = function(height, width) {
-  var x = height,
-    y = width;
-  function init() {
+  var matrix;
 
+  function init() {
+    // Create an empty world map with empty objects
+    var matrix = new Array(height);
+    for (var i = 0; i < height; i++){
+      matrix[i]= new Array(width);
+      for (var j = 0; j < width; j++){
+        matrix[i][j] = {
+          open: true
+        }
+      }
+    }
   }
+  init();
+  return matrix;
 };
 
-init();
+exports.World = World;
+
+
 
