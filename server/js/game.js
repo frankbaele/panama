267bd116669b4ca1e5797ae87a2790dcdb71fa9b
Frank Baele <frankbaele@gmail.com>
2013-08-01 08:35:07 +0200

    uncomment readline
    Former-commit-id: 7dd9f0c19b600ccf01dc2cf2f4caa91e0d5c4fb1

--- server/js/game.js
+++ server/js/game.js
@@ -12,13 +12,11 @@ var
 
 function init() {
   var tokens = ['height', 'width', 'port'];
-  /*
+  
   rlp(tokens).end(function(results){
     world = new World(results.height, results.width);
     connection = new SocketConnection(results.port, world);
-  });*/
-  world = new World(100, 100);
-  connection = new SocketConnection("8000", world.matrix);
+  });
 
 };
 
