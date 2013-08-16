2d9dcbcdddce07bda34388e4c258e025edae5579
Frank Baele <frankbaele@gmail.com>
2013-08-16 14:04:07 +0200

    hodor

--- server/js/game.js
+++ server/js/game.js
@@ -1,19 +1,19 @@
 var
-  // Node js requierements
+// Node js requierements
   _ = require('underscore'),
-  rlp = require('readline-prompter'),
+  $ = require('jquery'),
   // File variable includes
   World = require("./world").World,
   SocketConnection = require("./socketConnection").Connection,
   //Local variables
-  world,
+  generatedWorld,
   connection;
 
 
 function init() {
   'use strict';
-  world = new World(100, 100);
-  connection = new SocketConnection('8000', world);
+  generatedWorld = new World();
+  connection = new SocketConnection(8000, generatedWorld);
 }
 
 init();
