85a43a8aba2acbd1e773cfc787e43d6b69701932
Frank Baele <frankbaele@gmail.com>
2013-08-28 08:42:25 +0200

    breaking commit, first draft of world generation
    Former-commit-id: bac31111024c19291a2e609e1b5da8c04f2d2f52

--- server/js/world.js
+++ server/js/world.js
@@ -2,31 +2,110 @@ var World = function () {
   'use strict';
   var
     $ = require('jquery'),
-    matrix = new Object(),
-    json = require('../JSON/testmap.json');
+    height = 20,
+    width = 30,
+    tileHeight = 35,
+    tileWidth = 35,
+    mapData = [];
 
   function init() {
     generateWorld();
   }
   function generateWorld() {
-    // For now we parse a preconstructed world map from tiled,
-    // server should be able to choose between custom map or random map.
-    matrix.height = 20;
-    matrix.width = 30;
-    matrix.tileheight = 35;
-    matrix.tilewidth = 35;
-    matrix.data = [matrix.height];
-    for (var i = 0; matrix.height > i; i++) {
-      matrix.data[i] = [matrix.width];
-      for (var j = 0; matrix.width > j; j++){
-        matrix.data[i][j] = 0;
+
+  }
+  /**
+   * @class Cellular automaton map generator
+   * @augments ROT.Map
+   * @param {int} [width=ROT.DEFAULT_WIDTH]
+   * @param {int} [height=ROT.DEFAULT_HEIGHT]
+   * @param {object} [options] Options
+   * @param {int[]} [options.born] List of neighbor counts for a new cell to be born in empty space
+   * @param {int[]} [options.survive] List of neighbor counts for an existing  cell to survive
+   * @param {int} [options.topology] Topology 4 or 6 or 8
+   */
+  function Cellular(width, height, options) {
+    this._options = {
+      born: [5, 6, 7, 8],
+      survive: [4, 5, 6, 7, 8],
+      topology: 8
+    };
+    for (var p in options) { this._options[p] = options[p]; }
+
+    this._dirs = ROT.DIRS[this._options.topology];
+    this._map = this._fillMap(0);
+  }
+
+  /**
+   * Fill the map with random values
+   * @param {float} probability Probability for a cell to become alive; 0 = all empty, 1 = all full
+   */
+  function randomize(probability) {
+    for (var i=0;i<this.width;i++) {
+      for (var j=0;j<this.height;j++) {
+        this._map[i][j] = (getUniform() < probability ? 1 : 0);
       }
     }
+    return this;
   }
+
+  function set(x, y, value) {
+    this._map[x][y] = value;
+  }
+
+  function create(callback) {
+    var newMap = this._fillMap(0);
+    var born = this._options.born;
+    var survive = this._options.survive;
+
+
+    for (var j=0;j<this._height;j++) {
+      var widthStep = 1;
+      var widthStart = 0;
+      if (this._options.topology === 6) {
+        widthStep = 2;
+        widthStart = j%2;
+      }
+
+      for (var i=widthStart; i<this._width; i+=widthStep) {
+
+        var cur = this._map[i][j];
+        var ncount = this._getNeighbors(i, j);
+
+        if (cur && survive.indexOf(ncount) != -1) { /* survive */
+          newMap[i][j] = 1;
+        } else if (!cur && born.indexOf(ncount) != -1) { /* born */
+          newMap[i][j] = 1;
+        }
+
+        if (callback) { callback(i, j, newMap[i][j]); }
+      }
+    }
+
+    this._map = newMap;
+  };
+
+  /**
+   * Get neighbor count at [i,j] in this._map
+   */
+  function _getNeighbors(cx, cy) {
+    var result = 0;
+    for (var i=0;i<this._dirs.length;i++) {
+      var dir = this._dirs[i];
+      var x = cx + dir[0];
+      var y = cy + dir[1];
+
+      if (x < 0 || x >= this._width || x < 0 || y >= this._width) { continue; }
+      result += (this._map[x][y] == 1 ? 1 : 0);
+    }
+
+    return result;
+  }
+
   init();
 
   return {
-    matrix: matrix
+
   };
 };
 
