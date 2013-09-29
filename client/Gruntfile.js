5faf4b6000516d48287aac5842971a2d0aaf88c6
Frank Baele <frankbaele@gmail.com>
2013-09-29 11:48:43 +0200

    add node-webkit for the client
    Former-commit-id: a26ef42f26232ff19050ef460eda07bc5b37cd12

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -21,6 +21,16 @@ module.exports = function (grunt) {
       all: {
         path: 'http://localhost:9001'
       }
+    },
+    nodewebkit: {
+      options: {
+        build_dir: './dist', // Where the build version of my node-webkit app is saved
+        mac: true, // We want to build it for mac
+        win: true, // We want to build it for win
+        linux32: false, // We don't need linux32
+        linux64: false // We don't need linux64
+      },
+      src: ['./src/**/*'] // Your node-wekit app
     }
   });
 
