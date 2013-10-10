1ebcf79bc9970aae22327b7b8ff4dbb90f1e1d66
Frank Baele <frankbaele@gmail.com>
2013-10-10 07:26:48 +0200

    add asynch atlas sprite loader
    Former-commit-id: 4c2b4e0363cc2f87845a41b01be99bd8198bccaa

--- client/src/js/assetLoader.js
+++ client/src/js/assetLoader.js
@@ -1,37 +1,54 @@
 var assets = {
+  config: {
     mapTiles: {
-        type: "atlas",
-        configuration: "./art/panama.json",
-        file: "./art/panama.png"
+      type: "atlas",
+      configuration: "./art/panama.json",
+      file: "./art/panama.png"
     }
+  },
+  loaded : {
+    atlas : [],
+    music : []
+  }
 };
 
 var AssetLoaderConstructor = function () {
-    this.queue = 0;
+  this.queue = 0;
 };
 
 AssetLoaderConstructor.prototype.preloadAssets = function () {
-    var that = this;
-    // loop through the asset object and load each asset file
-    _.forEach(assets, function (asset) {
-        that.queue++;
-        switch (asset.type) {
-            case 'atlas':
-                that.loadAtlas(asset);
-                break;
+  var that = this;
+  // loop through the asset object and load each asset file
+  _.forEach(assets.config, function (asset) {
+    that.queue++;
+    switch (asset.type) {
+      case 'atlas':
+        that.loadAtlas(asset, function (loadedAsset) {
+          assets.loaded.atlas.push(loadedAsset);
+          that.queue--;
+        });
+        break;
 
-            case 'music':
-                break;
-        }
-    });
+      case 'music':
+        break;
+    }
+  });
 };
 
 AssetLoaderConstructor.prototype.preloadComplete = function () {
-    return this.queue > 0 ? false : true;
+  return this.queue > 0 ? false : true;
 };
-AssetLoaderConstructor.prototype.loadAtlas = function (data) {
-    var confRequest = jQuery.getJSON(data.configuration);
+AssetLoaderConstructor.prototype.loadAtlas = function (asset, callback) {
 
+  jQuery.getJSON(asset.configuration,function (data) {
+    asset.configuration = data;
+  }).then(function () {
+      var img = new Image();
+      img.src = asset.file;
+      asset.file = img;
+      callback(asset)
+    }
+  );
 };
 
 var assetLoader = new AssetLoaderConstructor();
