5a43072251d79aadfcb5819b5b2e0c5ef9200934
frankbaele <frankbaele@gmail.com>
2014-02-09 15:33:07 +0000

    add states
    Former-commit-id: 1f4e9682c49385e52f2dad95750814f91961504a

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,34 +1,39 @@
-define(['EventManager','angular', 'angular-ui-router'], function (eventManager, angular) {
+define(['EventManager', 'angular', 'angular-ui-router'], function (eventManager, angular) {
 
   var app = angular.module('panama', ['ui.router']);
 
-  app.config(function($stateProvider, $urlRouterProvider) {
-    //
-    // For any unmatched url, redirect to /state1
-    $urlRouterProvider.otherwise("/menu");
+  app.config(function ($stateProvider) {
     //
     // Now set up the states
     $stateProvider
       .state('menu', {
-        url: "/menu",
         templateUrl: "scripts/view/templates/menu.html"
       })
-      .state('canvas', {
-        url: "/game",
-        templateUrl: "scripts/view/templates/canvas.html",
-        controller: function($scope) {
-          eventManager.publish('game.init');
+      .state('game', {
+        abstract: true,
+        templateUrl: "scripts/view/templates/game.html"
+
+      })
+      .state('game.content', {
+        views: {
+          'canvas': {
+            templateUrl:'scripts/view/templates/game.canvas.html',
+            controller: function ($scope) {
+              eventManager.publish('game.init');
+            }
+          },
+          'actor': {
+            templateUrl:'scripts/view/templates/game.actor.html'
+          }
         }
       });
-
   });
+  app.run(['$state', function ($state) {
+    $state.transitionTo('menu');
+  }]);
 
   angular.element(document).ready(function () {
     angular.bootstrap(document, ['panama']);
   });
-  /*
-
-  );
-  */
   return app;
 });
