8d84b0f21423f3b33ccf19cee09a6cb4c40ff6d0
frankbaele <frankbaele@gmail.com>
2014-02-15 12:13:18 +0000

    remove dist
    Former-commit-id: 651c8730ad804095e846b769443b58023a770b39

--- dist/config/requireLiveConfig.js
+++
@@ -1,8 +0,0 @@
-require.config({
-  paths: {
-    jQuery: 'scripts/vendor/jquery-2.0.3.min',
-    underscore: 'scripts/vendor/lodash.min',
-    text: 'scripts/vendor/require.text',
-    keypress: 'scripts/vendor/keypress'
-  }
-});
