559a1e7bfe35cf87d6bc02e220e13162278b3c11
frankbaele <frankbaele@gmail.com>
2014-02-16 13:14:33 +0000

    fix require compile

--- config/requireLiveConfig.js
+++ config/requireLiveConfig.js
@@ -3,6 +3,24 @@ require.config({
     jQuery: 'scripts/vendor/jquery-2.0.3.min',
     underscore: 'scripts/vendor/lodash.min',
     text: 'scripts/vendor/require.text',
-    keypress: 'scripts/vendor/keypress'
-  }
+    keypress: 'scripts/vendor/keypress',
+    angular: 'scripts/vendor/angular.min',
+    'angular-ui-router': 'scripts/vendor/angular-ui-router.min'
+  },
+  shim: {
+    'angular' : {
+      'exports' : 'angular'
+    },
+    'angular-ui-router': {
+      deps:['angular']
+    },
+    'jQuery': {
+      'exports' : 'jQuery'
+    },
+    'underscore': {
+      exports: '_'
+    }
+  },
+  deps:['angular']
 });
+require( ['scripts/app']);
