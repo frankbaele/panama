3948ef5530d5c31710dc91170001a121598d8cb4
Frank Baele <frankbaele@gmail.com>
2015-03-08 00:22:22 +0100

    fix compile

--- config/requireLiveConfig.js
+++ config/requireLiveConfig.js
@@ -1,10 +1,10 @@
 requirejs.config({
     paths: {
-        text: './vendor/text',
-        Keypress: './vendor/keypress-2.1.0.min',
-        jquery: './vendor/dist/jquery',
-        lodash: './vendor/lodash',
-        q: './vendor/q'
+        text: 'scripts/vendor/text',
+        Keypress: 'scripts/vendor/keypress-2.1.0.min',
+        jquery: 'scripts/vendor/dist/jquery',
+        lodash: 'scripts/vendor/lodash',
+        q: 'scripts/vendor/q'
     },
     packages: [
 
