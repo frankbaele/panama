fad219626d91e49596f2eccff35e5d43cfb31000
Frank Baele <frankbaele@gmail.com>
2015-03-07 13:07:06 +0100

    fix assest loader and remove angularjs

--- config/requireLiveConfig.js
+++ config/requireLiveConfig.js
@@ -3,24 +3,15 @@ requirejs.config({
     jQuery: './scripts/vendor/jquery-2.0.3.min',
     underscore: './scripts/vendor/lodash.min',
     text: './scripts/vendor/require.text',
-    keypress: './scripts/vendor/keypress',
-    angular: './scripts/vendor/angular.min',
-    'angular-ui-router': './scripts/vendor/angular-ui-router.min'
+    keypress: './scripts/vendor/keypress'
   },
   shim: {
-    'angular' : {
-      'exports' : 'angular'
-    },
-    'angular-ui-router': {
-      deps:['angular']
-    },
     'jQuery': {
       'exports' : 'jQuery'
     },
     'underscore': {
       exports: '_'
     }
-  },
-  deps:['angular']
+  }
 });
 requirejs( ['./scripts/app']);
