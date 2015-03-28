6f422b38e8d3f2633c57c65a39a6cbdf930aca54
Frank Baele <frankbaele@gmail.com>
2015-03-28 21:23:57 +0100

    add first draft pixel movement and add polymer setup

--- config/requireLiveConfig.js
+++ config/requireLiveConfig.js
@@ -2,12 +2,12 @@ requirejs.config({
     paths: {
         text: 'scripts/vendor/text',
         Keypress: 'scripts/vendor/keypress-2.1.0.min',
-        jquery: 'scripts/vendor/dist/jquery',
+        jquery: 'scripts/vendor/jquery',
         lodash: 'scripts/vendor/lodash',
-        q: 'scripts/vendor/q'
+        q: 'scripts/vendor/q',
+        pathfinding: 'scripts/vendor/pathfinding-browser'
     },
     packages: [
 
     ]
 });
-requirejs(['./scripts/app']);
---
+++ scripts/config.js
@@ -0,0 +1,24 @@
+var app = {
+    config : {
+        actor:{
+            grid:{
+                width: 120,
+                height: 120
+            },
+            tile:{
+                width: 66,
+                height: 33
+            }
+        },
+        terrain : {
+            grid : {
+                width: 60,
+                height: 60
+            },
+            tile: {
+                width: 132,
+                height: 66
+            }
+        }
+    }
+};
