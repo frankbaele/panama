26631fe32df087a0d31ce34e5fb8715c7ab6938a
Frank Baele <frankbaele@gmail.com>
2015-03-30 22:16:39 +0200

    finer movement

--- config/requireLiveConfig.js
+++ config/requireLiveConfig.js
@@ -2,7 +2,7 @@ requirejs.config({
     paths: {
         text: 'scripts/vendor/text',
         Keypress: 'scripts/vendor/keypress-2.1.0.min',
-        jquery: 'scripts/vendor/jquery',
+        jquery: 'scripts/vendor/dist/jquery',
         lodash: 'scripts/vendor/lodash',
         q: 'scripts/vendor/q',
         pathfinding: 'scripts/vendor/pathfinding-browser'
@@ -11,3 +11,4 @@ requirejs.config({
 
     ]
 });
+requirejs(['./scripts/app']);
