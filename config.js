92738fad915e89f8c3f0f2b6ccda55c341ea1e2b
Frank Baele <frankbaele@gmail.com>
2015-03-31 21:40:50 +0200

    update build

---
+++ config.js
@@ -0,0 +1,26 @@
+var app = {
+    config : {
+        framerate: 15,
+        cycle: 200,
+        actor:{
+            grid:{
+                width: 120,
+                height: 120
+            },
+            tile:{
+                width: 66,
+                height: 33
+            }
+        },
+        terrain : {
+            grid : {
+                width: 60,
+                height: 60
+            },
+            tile: {
+                width: 132,
+                height: 66
+            }
+        }
+    }
+};
