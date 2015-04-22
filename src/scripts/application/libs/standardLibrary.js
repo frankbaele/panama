dd42c15f521684809e95f92ce4f52bae28f32ea1
Frank Baele <frankbaele@gmail.com>
2015-04-22 16:07:11 +0200

    fix standard lib

--- src/scripts/application/libs/standardLibrary.js
+++ src/scripts/application/libs/standardLibrary.js
@@ -3,44 +3,59 @@ define(['eventmanager'], function (eventmanager) {
         return Math.floor((1 + Math.random()) * 0x10000)
             .toString(16)
             .substring(1);
-    };
+    }
 
     function guid() {
         return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
             s4() + '-' + s4() + s4() + s4();
     }
 
-    function isoToTwoD(posX, posY) {
-        var newCoordinates = {};
-        newCoordinates.x = (posY + posX);
-        newCoordinates.y = (posY - posX);
-        return newCoordinates;
-    };
+    function isoToTwoD(config) {
+        var x = (config.x + config.y) / 2;
+        var y = (config.y - (config.x)) / 2;
+        return ({x:x,y:y});
+    }
 
-    function twoDToIso(posX, posY) {
+    function twoDToIso(config) {
         var newCoordinates = {};
-        newCoordinates.x = (posX - posY) / 2;
-        newCoordinates.y = (posX + posY) / 2;
+        newCoordinates.x = (config.x - config.y) / 2;
+        newCoordinates.y = (config.x + config.y) / 2;
         return newCoordinates;
     }
 
     function worldPosToGridPos(config) {
         config = _.cloneDeep(config);
+        // Add correction for the centering of the map
+        config.x = config.x - (app.config.actor.grid.width/2 * app.config.actor.tile.width);
+        var coords = isoToTwoD(config);
+        coords.x = coords.x/(app.config.actor.car.width/2);
+        coords.y = coords.y/(app.config.actor.car.height/2);
+        coords.x = Math.floor(coords.x);
+        coords.y = Math.floor(coords.y);
+        return (coords);
+    }
+    function worldPosToIsoPos(config){
+        var gridCoordinates = worldPosToGridPos(config);
+        return twoDToIso({x: gridCoordinates.x, y:gridCoordinates.y});
+    }
+    function isoWorldPosToCarWorldPos(config){
+        config = _.cloneDeep(config);
         config.x = config.x - (app.config.actor.grid.width/2 * app.config.actor.tile.width);
         // Add correction for the centering of the map
-        var x = (config.x/ (app.config.actor.tile.width / 2) + config.y / (app.config.actor.tile.height / 2)) / 2;
-        var y = (config.y/ (app.config.actor.tile.height / 2) - (config.x / (app.config.actor.tile.width / 2))) / 2;
-        x = Math.floor(x);
-        y = Math.floor(y);
+        var coords = isoToTwoD(config);
+        return (coords);
+    }
+    function carWorldPosToIsoWorldPos(config){
+        config = _.cloneDeep(config);
+        // Add correction for the centering of the map
+        var x = config.y + config.x;
+        var y = config.y - config.x;
+        x = x + (app.config.actor.grid.width/2 * app.config.actor.tile.width);
         return ({x:x,y:y});
     }
 
-    function worldPosToIsoPos(config){
-        var gridCoordinates = worldPosToGridPos(config);
-        return twoDToIso(gridCoordinates.x, gridCoordinates.y);
-    }
     function gridPosToWorldPos(config){
-        var iso = twoDToIso(config.x, config.y);
+        var iso = twoDToIso({x: config.x, y:config.y});
         var y = iso.y * app.config.actor.tile.height;
         var x = iso.x * app.config.actor.tile.width + (app.config.actor.grid.width/2 * app.config.actor.tile.width);
         return{
@@ -55,11 +70,8 @@ define(['eventmanager'], function (eventmanager) {
                 clearInterval(tev);
             }
         }, 1000);
-    };
-    function roundHalf(num) {
-        num = Math.round(num * 2) / 2;
-        return num;
     }
+
 
     return {
         guid: guid,
@@ -68,6 +80,8 @@ define(['eventmanager'], function (eventmanager) {
         worldPosToGridPos: worldPosToGridPos,
         worldPosToIsoPos: worldPosToIsoPos,
         gridPosToWorldPos: gridPosToWorldPos,
+        isoWorldPosToCarWorldPos: isoWorldPosToCarWorldPos,
+        carWorldPosToIsoWorldPos: carWorldPosToIsoWorldPos,
         checkWait: checkWait
     };
 });
