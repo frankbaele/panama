47185604b2eee3fcb9490a5b13847b4b69516f05
Frank Baele <frankbaele@gmail.com>
2015-05-25 17:55:54 +0200

    add maps

--- config/requireLiveConfig.js
+++ config/requireLiveConfig.js
@@ -2,7 +2,7 @@ requirejs.config({
     paths: {
         text: 'scripts/vendor/text',
         Keypress: 'scripts/vendor/keypress-2.1.0.min',
-        jQuery: 'scripts/vendor/jquery',
+        jquery: 'scripts/vendor/jquery',
         lodash: 'scripts/vendor/lodash',
         q: 'scripts/vendor/q',
         pathfinding: 'scripts/vendor/pathfinding-browser',
